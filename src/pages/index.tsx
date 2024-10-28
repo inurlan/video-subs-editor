7ea131473269b117d1b11fec62a825a45bed6e22
Nurlan Ilyasov <nilyasov2018@gmail.com>
2024-10-29 00:10:19 +0400

    mobile responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import MediaSyncProvider from '@/context/MediaSyncProvider';
-import '../styles/Home.module.scss';
 import { CssBaseline, Container, Grid2, Paper } from '@mui/material';
 import React from 'react';
 import AudioTrack from '@/components/audio/AudioTrack';
@@ -12,15 +11,15 @@ export default function Home() {
       <CssBaseline />
       <Container maxWidth="xl">
         <Grid2 container spacing={2}>
-          <Grid2 size={6}>
+          <Grid2 size={{ xs: 12, md: 6 }} style={{ height: '70vh' }}>
             <Subtitles />
           </Grid2>
-          <Grid2 size={6}>
+          <Grid2 size={{ xs: 12, md: 6 }}>
             <Paper style={{ height: '70vh', width: '100%' }}>
               <VideoPlayer />
             </Paper>
           </Grid2>
-          <Grid2 size={12}>
+          <Grid2 size={{ xs: 12, md: 12 }}>
             <Paper style={{ height: '26vh', width: '100%' }}>
               <AudioTrack />
             </Paper>
